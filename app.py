ac06507ffaff1a2c9232a7c6224f5a3a6da2368f
Jiayuan <forrestchang7@gmail.com>
2019-04-08 17:49:35 +0800

    feat: move application to another file

--- app.py
+++ app.py
@@ -1,18 +1,10 @@
 # -*- coding: utf-8 -*-
 
 """
-    Zen, a simple web framework.
+    A wsgi application.
 """
 
-
-class Zen:
-    def __call__(self, environ, start_response):
-        response_body = b'Hello, World'
-        status = '200 OK'
-        headers = [('HELLO', 'WORLD')]
-        start_response(status, headers)
-        return [response_body]
-
+from zen import Zen
 
 app = Zen()
 
---
+++ zen.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""
+    Zen, a simple web framework.
+"""
+
+
+class Zen:
+    def __call__(self, environ, start_response):
+        response_body = b'Hello, World'
+        status = '200 OK'
+        headers = [('HELLO', 'WORLD')]
+        start_response(status, headers)
+        return [response_body]
+

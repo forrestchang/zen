17e03d902e672a62fb6314ecab4f23db537b5010
Jiayuan <forrestchang7@gmail.com>
2019-05-23 14:36:19 +0800

    style: format server.py

--- server.py
+++ server.py
@@ -1,10 +1,9 @@
 """server.py"""
 
 from wsgiref.simple_server import make_server
-from app import app
 
+from app import app
 
 if __name__ == '__main__':
     server = make_server('127.0.0.1', 8888, app)
     server.serve_forever()
-

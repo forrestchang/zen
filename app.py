a8ea65f29f8083675085e1d953d63dccead8514f
Jiayuan <forrestchang7@gmail.com>
2019-05-24 10:31:10 +0800

    style: format with black

--- app.py
+++ app.py
@@ -6,32 +6,33 @@ from zen import Zen
 app = Zen()
 
 
-@app.route('/', method='GET')
+@app.route("/", method="GET")
 def index(request, response):
-    return 'Hello world'
+    return "Hello world"
 
 
-@app.route('/path', method='GET')
+@app.route("/path", method="GET")
 def path(request, response):
     return request.path
 
 
-@app.route('/fib', method='GET')
+@app.route("/fib", method="GET")
 def fib(request, response):
-    num = request.query.get('num')
+    num = request.query.get("num")
     print(request.query_string)
 
     if num is None:
-        return 'Could not find num.'
+        return "Could not find num."
 
     return fib_service(int(num))
 
 
-@app.route('/time', method='GET')
+@app.route("/time", method="GET")
 def time(request, response):
-    now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    response.set_header('TIME', now)
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    response.set_header("TIME", now)
     return now
+
 
 def fib_service(num):
     if num == 0:
@@ -40,4 +41,3 @@ def fib_service(num):
         return 1
     else:
         return fib_service(num - 1) + fib_service(num - 2)
-
--- server.py
+++ server.py
@@ -4,6 +4,6 @@ from wsgiref.simple_server import make_server
 
 from app import app
 
-if __name__ == '__main__':
-    server = make_server('127.0.0.1', 8888, app)
+if __name__ == "__main__":
+    server = make_server("127.0.0.1", 8888, app)
     server.serve_forever()
